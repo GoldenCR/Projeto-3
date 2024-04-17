189e2a6ad92286177a4834caa4b2a72851da1c87
GoldenCR <160672401+GoldenCR@users.noreply.github.com>
2024-04-17 19:36:55 +0000

    Adicionar e mostrar nao roda ainda mas parece estar certo

--- main.c
+++ main.c
@@ -1,4 +1,49 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct Usuario {
+    char nome[50];
+    char sobrenome[50];
+    char email[100];
+    char telefone[20];
+};
+
+void adicionarUsuario(struct Usuario **lista, int *numUsuarios) {
+    *lista = realloc(*lista, (*numUsuarios + 1) * sizeof(struct Usuario));
+    if (*lista == NULL) {
+        printf("Erro ao alocar memória.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Digite o nome do usuario: ");
+    scanf("%s", (*lista)[*numUsuarios].nome);
+
+    printf("Digite o sobrenome do usuario: ");
+    scanf("%s", (*lista)[*numUsuarios].sobrenome);
+
+    printf("Digite o email do usuario: ");
+    scanf("%s", (*lista)[*numUsuarios].email);
+
+    printf("Digite o telefone do usuario: ");
+    scanf("%s", (*lista)[*numUsuarios].telefone);
+
+    (*numUsuarios)++;
+}
+void mostrarUsuarios(struct Usuario *lista, int numUsuarios) {
+    if (numUsuarios == 0) {
+        printf("Lista de Usuários está vazia.\n");
+        return;
+    }
+    printf("Lista de Usuários:\n");
+    for (int i = 0; i < numUsuarios; i++) {
+        printf("%d. Nome: %s\n", i+1, lista[i].nome);
+        printf("   Sobrenome: %s\n", lista[i].sobrenome);
+        printf("   Email: %s\n", lista[i].email);
+        printf("   Telefone: %s\n", lista[i].telefone);
+        printf("\n");    
+    }
+}
 
 int main() {
     struct Usuario *listaUsuarios = NULL;
